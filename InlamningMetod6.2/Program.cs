39f12f1904e4a0c9c3bd8b83f29fb24d758032b3
Airblow <143172177+Airblow@users.noreply.github.com>
2024-02-28 10:04:11 +0100

    Add project files.

---
+++ InlamningMetod6.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InlamningMetod6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal1 = 15;
+            int tal2 = 23;
+
+            int largestNumber = Störst(tal1, tal2);
+            Console.WriteLine($"Det största talet utav {tal1} och {tal2} är {largestNumber}");
+
+            Console.ReadKey();
+
+        }
+
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 >= tal2)
+            {
+                return tal1;
+            }
+            else
+            {
+                return tal2;
+            }
+
+        }
+
+    }
+
+}
